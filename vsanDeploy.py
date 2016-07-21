c2e4575b8a60f2a629fe73ef163dac6147d94877
Storage Code <vmwstoragecode@gmail.com>
2016-07-21 13:35:34 -0500

    Update vsanDeploy.py

--- vsanDeploy.py
+++ vsanDeploy.py
@@ -5,8 +5,18 @@
 Copyright 2016 VMware, Inc.  All rights reserved.
 
 This file includes sample codes for configure VSAN cluster.
+
 Before running this script, please make sure you have a vc cluster and proper VSAN licence
 
+The code has been tested with the following configuration
+Testbed: One cluster with four hosts. Each host has one 50G SSD and two 100G SSD
+Preconditions:
+1. Please make sure your VSAN's license contains All-Flash if deploying All-Flash
+2. The cluster does not have VSAN turned on
+3. The hosts are part of a vsphere Cluster
+
+Case1: Create a VSAN cluster 
+python vsanDeploy.py -s <VCENTERSERVER> -u user -p password --cluster CLUSTER --allflash --vmknic vmkX --vsanlicense 
 
 """
 
